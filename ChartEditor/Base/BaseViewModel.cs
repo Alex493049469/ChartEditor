d25e2d03bccb822354888896fadf5a904f8df0fe
Alex493049469 <alex493049469@yandex.ru>
2015-07-12 12:31:05 +0700

    последние изменения

---
+++ ChartEditor/Base/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartEditor.Base
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        #region MVVM related
+        protected void RaisePropertyChanged([CallerMemberName]string propertyName = "") // волшебство .NET 4.5
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+    }
+}
